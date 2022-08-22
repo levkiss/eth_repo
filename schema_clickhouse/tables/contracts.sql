c1b3b176ff089eb1d3578fd0074f27a26a235be2
levkiss <>
2022-08-22 20:26:11 +0300

    Upd

--- schema_clickhouse/tables/contracts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE STG_ETH.CONTRACTS
-(
-    address     FixedString(70),
-    description String,
-    is_erc20    UInt8,
-    is_erc721   UInt8
-) ENGINE = MergeTree()
-order by address;
--- schema_clickhouse/tables/contracts_custom.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE STG_ETH.CONTRACTS (
-    contract_address FixedString(70),
-    contract_name String
-)
-ENGINE Log()
